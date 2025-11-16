290e067ad84a7820d20fa2c75eb0b682496cf9a0
RominM <romin@valuemycar.fr>
2025-11-16 20:55:37 +0100

    fix: navigation

--- components/navigation/nav-list.vue
+++ components/navigation/nav-list.vue
@@ -1,12 +1,21 @@
 <template>
   <ul class='nav-list'>
-    <button-nav v-for="route in routes" :key="route?.name" :route="route" />
+    <button-nav v-for="route in sortedRoutes" :key="route?.name" :route="route" />
   </ul>
 </template>
 
 <script setup lang='ts'>
-import type { RouteRecordNormalized } from 'vue-router'
-defineProps({routes: {type: Array as PropType<RouteRecordNormalized[]>, required: true}})
+import type { RouteRecordNormalized } from 'vue-router';
+const props = defineProps({ routes: { type: Array as PropType<RouteRecordNormalized[]>, required: true } })
+
+const desiredOrder = ['projects', 'about', 'experiences', 'contact']
+
+const sortedRoutes = computed(() =>
+  desiredOrder
+    .map(name => props.routes.find(r => r.name === name))
+    .filter(Boolean) as RouteRecordNormalized[]
+)
+
 </script>
 
 <style lang='scss' scoped>
